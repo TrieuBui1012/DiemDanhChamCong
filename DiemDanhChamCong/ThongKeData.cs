72ea7928caa286e19e20beb3727cff3b69eeee35
taeitoilet <taeitoilet@gmail.com>
2023-12-08 21:32:48 +0700

    Quý Thống Kê
    Con lơn Quang

---
+++ DiemDanhChamCong/ThongKeData.cs
@@ -0,0 +1,30 @@
+﻿using DiemDanhChamCong.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using ZXing;
+using ZXing.Maxicode;
+using ZXing.Windows.Compatibility;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+
+namespace DiemDanhChamCong
+{
+    class ThongKeData
+    {
+            public double? LateHours { get; set; }
+            public double? EarlyHours { get; set; }
+
+            public string? MaNV { get; set; }
+            public string? TenNV { get; set; }
+
+    }
+    
+
+}
---
+++ DiemDanhChamCong/XemChiTiet.cs
@@ -0,0 +1,51 @@
+﻿using DiemDanhChamCong.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiemDanhChamCong
+{
+    class XemChiTiet
+    {
+        public ChamCongContext db = new ChamCongContext();
+        public class XemChiTietTT
+        {
+            public long Idcc { get; set; }
+
+            public DateTime Vao { get; set; }
+
+            public DateTime Ra { get; set; }
+
+            public long? SoPhutMuon { get; set; }
+
+            public long? SoPhutSom { get; set; }
+
+            public string? MaNv { get; set; }
+
+            public string LoaiCong { get; set; }
+
+            public string LoaiCa{ get; set;}
+            public string TenNv { get; set; }
+        }
+        public List<XemChiTietTT> LayDL( string manv , DateTime startdate, DateTime enddate)
+        {
+            var query = from t in db.Chamcongs
+                        where t.MaNv == manv && startdate.Date <= t.Vao.Date && t.Vao.Date <= enddate.Date
+                        select new XemChiTietTT
+                        {
+                            Idcc = t.Idcc,
+                            Vao = t.Vao,
+                            Ra = t.Ra,
+                            SoPhutMuon = t.SoPhutMuon,
+                            SoPhutSom = t.SoPhutSom,
+                            MaNv = t.MaNv,
+                            LoaiCong = (db.Loaicongs.SingleOrDefault(lc => lc.IdloaiCong == t.IdloaiCong) == null) ? "Không" : db.Loaicongs.SingleOrDefault(lc => lc.IdloaiCong == t.IdloaiCong).TenLoaiCong,
+                            LoaiCa = (db.Loaicas.SingleOrDefault(lc => lc.IdloaiCa == t.IdloaiCa) == null) ? "Không" : db.Loaicas.SingleOrDefault(lc => lc.IdloaiCa == t.IdloaiCa).TenLoaiCa,
+                            TenNv = db.Nhanviens.SingleOrDefault(ten => ten.MaNv == t.MaNv).HoTen
+                        };
+            return query.ToList<XemChiTietTT>();
+        }
+    }
+}
